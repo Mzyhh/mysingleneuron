0169a073b9ba4741d92ff90297ec68d435467d85
Mzyhh <118711941+Mzyhh@users.noreply.github.com>
2024-10-22 00:17:18 +0300

    Change error handling and other stuffs

--- include/matrix.h
+++ include/matrix.h
@@ -5,17 +5,23 @@
 #include <stdio.h>
 
 #define WRONG_SHAPE 52
+#define OUT_OF_BOUNDARIES 69
+#define ALLOCATION_ERROR 11
 
 
 typedef struct {
-    float **matrix;
+    double *matrix;
     size_t rows;
     size_t columns;
 } matrix;
 
-int matcreate(const size_t rows, const size_t columns, matrix *const result);
-int matmul(const matrix m1, const matrix m2, matrix *const result);
-int matadd(const matrix m1, const matrix m2, matrix *const result);
-int matprint(const matrix m);
+double get_index(const matrix *m, size_t i, size_t j);
+void set_index(matrix *const m, const double value, size_t i, size_t j);
+void set_err(int code);
+int get_err();
+matrix* matcreate(const size_t rows, const size_t columns);
+matrix* matmul(const matrix *m1, const matrix *m2);
+matrix* matadd(const matrix *m1, const matrix *m2);
+int matprint(const matrix *m);
 
 #endif //_MATRIX_H
--- matrix.c
+++ matrix.c
@@ -1,17 +1,50 @@
 #include "include/matrix.h"
 
-int matcreate(const size_t rows, const size_t columns, matrix *const result)
+static int error = EXIT_SUCCESS;
+
+void set_err(int code)
+{
+    error = code;
+}
+
+int get_err()
+{
+    int tmp = error;
+    error = EXIT_SUCCESS;
+    return tmp;
+}
+
+double get_index(const matrix *m, size_t i, size_t j)
+{
+    if (i < 0 || i >= m->rows || j < 0 || j >= m->columns) {
+        set_err(OUT_OF_BOUNDARIES);
+    }
+    return m->matrix[i*m->rows + j];
+}
+
+void set_index(matrix *m, const double value, size_t i, size_t j)
+{
+    if (i < 0 || i >= m->rows || j < 0 || j >= m->columns) {
+        set_err(OUT_OF_BOUNDARIES);
+    }
+    m->matrix[i*m->rows + j] = value;
+}
+
+matrix* matcreate(const size_t rows, const size_t columns)
 {
     if (rows <= 0 || columns <= 0) {
-        return WRONG_SHAPE;
+        set_err(WRONG_SHAPE);
     }
+    matrix* result = (matrix*)malloc(sizeof(matrix));
+    if (!result) {
+        set_err(ALLOCATION_ERROR);
+        return result;
+    }
+
     result->rows = rows;
     result->columns = columns;
-    result->matrix = (float**)calloc(rows, sizeof(float*));
-    for (size_t i = 0; i < rows; ++i) {
-        result->matrix[i] = (float*)calloc(columns, sizeof(float));
-    }
-    return EXIT_SUCCESS;
+    result->matrix = (double*)calloc(rows*columns, sizeof(double*));
+    return result;
 }
 
 /**
@@ -20,26 +53,26 @@ int matcreate(const size_t rows, const size_t columns, matrix *const result)
  * Resulting matrix has shape (m, n).
  * @param result should be pointer on existing matrix with no allocated memory under field matrix.
  */
-int matmul(const matrix m1, const matrix m2, matrix *const result)
+matrix* matmul(const matrix *m1, const matrix *m2)
 {
-    if (m1.columns != m2.rows) {
-        return WRONG_SHAPE;
+    if (m1->columns != m2->rows) {
+        set_err(WRONG_SHAPE);
+        return NULL;
     }
 
-    int status = matcreate(m1.rows, m2.columns, result); 
-    if (status != EXIT_SUCCESS) {
-        return status;
-    }
-
-    for (size_t i = 0; i < m1.rows; ++i) {
-        for (size_t j = 0; j < m2.columns; ++j) {
-            result->matrix[i][j] = 0.0f;
-            for (size_t k = 0; k < m1.rows; ++k) {
-                result->matrix[i][j] += m1.matrix[i][k] * m2.matrix[k][j];
+    matrix *result = matcreate(m1->rows, m2->columns); 
+    if (result) {
+        for (size_t i = 0; i < m1->rows; ++i) {
+            for (size_t j = 0; j < m2->columns; ++j) {
+                set_index(result, 0, i, j);
+                for (size_t k = 0; k < m1->rows; ++k) {
+                    const int c = get_index(m1, i, k)*get_index(m2, k, j);
+                    set_index(result, get_index(result, i, j) + c, i, j);
+                }
             }
         }
     }
-    return EXIT_SUCCESS;
+    return result;
 }
 
 /**
@@ -47,31 +80,30 @@ int matmul(const matrix m1, const matrix m2, matrix *const result)
  * Matrices m1 and m2 should be of the same shape.
  * @param result should be pointer on existing matrix with no allocated memory under field matrix.
  */
-int matadd(const matrix m1, const matrix m2, matrix *const result)
+matrix* matadd(const matrix *m1, const matrix *m2)
 {
-    if (m1.rows != m2.rows || m1.columns != m2.columns) {
-        return WRONG_SHAPE;
+    if (m1->rows != m2->rows || m1->columns != m2->columns) {
+        set_err(WRONG_SHAPE);
+        return NULL;
     }
     
-    int status = matcreate(m1.rows, m1.columns, result);
-    if (status != EXIT_SUCCESS) {
-        return status;
-    }
-
-    for (size_t i = 0; i < result->rows; ++i) {
-        for (size_t j = 0; j < result->columns; ++j) {
-            result->matrix[i][j] = m1.matrix[i][j] + m2.matrix[i][j];
+    matrix *result = matcreate(m1->rows, m1->columns); 
+    if (result) {
+        for (size_t i = 0; i < result->rows; ++i) {
+            for (size_t j = 0; j < result->columns; ++j) {
+                set_index(result, get_index(m1, i, j) + get_index(m2, i, j), i, j);
+            }
         }
     }
 
-    return EXIT_SUCCESS;
+    return result;
 }
 
-int matprint(const matrix m) 
+int matprint(const matrix *m) 
 {
-    for (size_t i = 0; i < m.rows; ++i) {
-        for (size_t j = 0; j < m.columns; ++j) {
-            printf("%f\t", m.matrix[i][j]);
+    for (size_t i = 0; i < m->rows; ++i) {
+        for (size_t j = 0; j < m->columns; ++j) {
+            printf("%f\t", get_index(m, i, j));
         }
         printf("\n");
     }
@@ -80,31 +112,30 @@ int matprint(const matrix m)
 
 int main(void) 
 {
-    matrix a;
-    matcreate(2, 2, &a); 
-    a.matrix[0][0] = 1; a.matrix[0][1] = 2;
-    a.matrix[1][0] = 3; a.matrix[1][1] = 4;
+    matrix *a = matcreate(2, 2); 
+    set_index(a, 1, 0, 0);
+    set_index(a, 2, 0, 1);
+    set_index(a, 3, 1, 0);
+    set_index(a, 4, 1, 1);
     matprint(a); 
     printf("\n");
 
-    matrix b;
-    matcreate(2, 2, &b); 
-    b.matrix[0][0] = -1; b.matrix[0][1] = 2;
-    b.matrix[1][0] = 5; b.matrix[1][1] = 4;
+    matrix *b = matcreate(2, 2); 
+    set_index(b, -1, 0, 0);
+    set_index(b, 5, 0, 1);
+    set_index(b, -3, 1, 0);
+    set_index(b, 0, 1, 1);
     matprint(b); 
     printf("\n");
 
-    matrix c;
-    matmul(a, b, &c);
-    matprint(c);
+//    matrix *c = matadd(a, b);
+//    matprint(c); 
+//    printf("\n");
+
+    matrix *d = matmul(a, b);
+    matprint(d); 
     printf("\n");
 
-    matrix d;
-    int ret = matadd(a, b, &d);
-    if (ret != EXIT_SUCCESS) {
-        printf("Error code is %d\n", ret);
-    } else {
-        matprint(d);
-        printf("\n");
-    }
+
+    return error;
 }
