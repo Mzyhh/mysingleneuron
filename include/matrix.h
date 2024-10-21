56393f1c625f65a469a76f961e04f0c608d6595e
Mzyhh <118711941+Mzyhh@users.noreply.github.com>
2024-10-21 22:10:38 +0300

    Some first steps in creating library for working with matrices.

---
+++ include/matrix.h
@@ -0,0 +1,20 @@
+#ifndef _MATRIX_H
+#define _MATRIX_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define WRONG_SHAPE 52
+
+
+typedef struct {
+    float **matrix;
+    size_t rows;
+    size_t columns;
+} matrix;
+
+int matcreate(const size_t rows, const size_t columns, matrix *const result);
+int matmul(const matrix m1, const matrix m2, matrix *const result);
+int matprint(const matrix m);
+
+#endif //_MATRIX_H
