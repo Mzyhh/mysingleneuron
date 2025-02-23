578674c495be53ae4232a2756cae8185e29261dd
Mzyhh <xiiad@mail.ru>
2025-02-23 19:17:03 +0300

    Optimize getToken function

--- src/parser.c
+++ src/parser.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-#include <math.h>
 
 #include "stringstream.h"
 #include "tree.h"
@@ -10,43 +9,37 @@ int isvalidVarChar(char c) {
     return isalpha(c) || c == '_';
 }
 
-Token *getToken(struct stringstream *ss) {
+int getToken(struct stringstream *ss, Token* dest) {
     int old_offset = ss->offset;
     while (isspace(ss->data[ss->offset]) && !ss->isempty(ss)) ss->offset++;
     if(ss->isempty(ss)) {
-        return NULL;
+        return 1;
     }
     size_t start = ss->offset;
-    Token *t;
     if (isalpha(ss->data[start])) {
         while (isvalidVarChar(ss->data[ss->offset]) && !ss->isempty(ss)) ss->offset++;
         size_t end = ss->offset;
-        t = (Token*)malloc(sizeof(Token) + sizeof(char)*(end-start));
-        t->type = Var;
-        t->varname = (char*)(t+sizeof(Token));
-        strncpy(t->varname, ss->data + start, end - start);
+        dest->type = Var;
+        strncpy(dest->data.varname, ss->data + start, end - start);
     } else if (isdigit(ss->data[start])) {
-        t = (Token*)malloc(sizeof(Token));
-        t->type = Num;
-        t->number = atof(ss->data + start);
+        dest->type = Num;
+        dest->data.number = atof(ss->data + start);
         while(isdigit(ss->data[ss->offset]) || ss->data[ss->offset] == '.') 
             ss->offset++;
          
     } else if (ss->data[start] == '(') {
-        t = (Token*)malloc(sizeof(Token));
-        t->type = Open;
+        dest->type = Open;
         ss->offset++;
     } else if (ss->data[start] == ')') {
-        t = (Token*)malloc(sizeof(Token));
-        t->type = Close;
+        dest->type = Close;
         ss->offset++;
     } else {
-        t = (Token*)malloc(sizeof(Token) + sizeof(char));
-        t->type = Op;
-        t->varname = (char*)(t + sizeof(Token));
-        t->varname[0] = ss->data[ss->offset++];
+        dest->type = Op;
+        dest->data.varname[0] = ss->data[ss->offset++];
+        dest->data.varname[1] = '\0';
     }
-    return t;
+
+    return EXIT_SUCCESS;
 }
 
 tNode *createSemanticTree(stringstream *ss) {
@@ -63,19 +56,18 @@ int main() {
     size_t n = 128;
     getline(&s, &n, stdin); 
     INIT_SS(ss, s);
-    Token *t;
-    while ((t = getToken(&ss))) {
-        if (t->type == Var) {
-            printf("Var: %s\n", t->varname);
-        } else if(t->type == Num) {
-            printf("Num: %lf\n", t->number);
-        } else if(t->type == Open) {
+    Token t;
+    while (!getToken(&ss, &t)) {
+        if (t.type == Var) {
+            printf("Var: %s\n", t.data.varname);
+        } else if(t.type == Num) {
+            printf("Num: %lf\n", t.data.number);
+        } else if(t.type == Open) {
             printf("Open bracket\n");
-        } else if(t->type == Close) {
+        } else if(t.type == Close) {
             printf("Close bracket\n");
         } else {
-            printf("Op: %s\n", t->varname);
+            printf("Op: %s\n", t.data.varname);
         }
-        free(t);
     }
 }
--- src/tree.h
+++ src/tree.h
@@ -1,17 +1,19 @@
 #ifndef __TREE_H__
 #define __TREE_H__
 
+#define MAX_TOKEN_LEN 16
 #define CREATE_LEAF(node, item)\
     tNode *node = (tNode*)malloc(sizeof(tNode));\
     node->item = item;\
     node->left = NULL;\
     node->right = NULL;
 
-enum Type {Op, Num, Var, Open, Close};
 typedef struct parserToken {
-    enum Type type;
-    char *varname;
-    double number;
+    enum {Op, Num, Var, Open, Close} type;
+    union {
+        char varname[MAX_TOKEN_LEN];
+        double number;
+    } data;
 } Token;
 
 typedef struct treeNode {
